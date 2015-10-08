7d05d3ad4343fff98108055fa7252ae92c566a0a
Paul Beresuita <paul.beresuita@gmail.com>
2015-10-08 00:04:50 -0400

    Added lib that will allow youtube videos to be responsive

--- www/app/app.js
+++ www/app/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('sparked', ['ionic'])
+angular.module('sparked', ['ionic', 'youtube-embed'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -46,7 +46,8 @@ angular.module('sparked', ['ionic'])
 
       .state('home', {
         url: '/home',
-        templateUrl: 'app/home/views/home.html'
+        templateUrl: 'app/home/views/home.html',
+        controller: 'HomeController'
       })
 
       .state('connect', {
--- www/app/home/controllers/homeController.js
+++ www/app/home/controllers/homeController.js
@@ -1,3 +1,20 @@
-/**
- * Created by paulberesuita on 10/6/15.
- */
+(function() {
+
+    'use strict';
+
+    angular.module('sparked').controller('HomeController', HomeController);
+
+    HomeController.$inject = ['$scope'];
+
+    function HomeController($scope) {
+
+        var vm = this;
+
+        $scope.videoOneURL = 'https://www.youtube.com/embed/kYfNvmF0Bqw';
+        $scope.videoTwoURL = 'https://www.youtube.com/watch?t=305&v=mgmVOuLgFB0';
+
+
+    };
+
+
+})();
