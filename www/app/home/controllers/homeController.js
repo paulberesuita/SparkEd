73d9cc44a284138d969f038c898156df744f2a5a
Paul Beresuita <paul.beresuita@gmail.com>
2015-10-17 13:27:11 -0400

    Fix issue with displaying youtube videos

--- www/app/home/controllers/homeController.js
+++ www/app/home/controllers/homeController.js
@@ -4,11 +4,13 @@
 
     angular.module('sparked').controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope'];
+    HomeController.$inject = ['$scope', '$sce'];
 
-    function HomeController($scope) {
+    function HomeController($scope, $sce) {
 
         var vm = this;
+
+        //$scope.linktest = $sce.trustAsResourceUrl('https://www.youtube.com/embed/YU2LP2QpUaE');
 
         vm.allcontent = [
             {name: "Video 1 Title", link:'https://www.youtube.com/embed/YU2LP2QpUaE'},
@@ -20,6 +22,9 @@
         $scope.videoOneURL = 'https://www.youtube.com/embed/kYfNvmF0Bqw';
         $scope.videoTwoURL = 'https://www.youtube.com/watch?t=305&v=mgmVOuLgFB0';
 
+        $scope.trustSrc = function(src) {
+            return $sce.trustAsResourceUrl(src);
+        }
 
     };
 
