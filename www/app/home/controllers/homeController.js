eb96fd66713c890cb847275fd4c3b151ec3020c7
Paul Beresuita <paul.beresuita@gmail.com>
2015-10-29 22:10:20 -0400

    Displaying courses based on topics selected by user (needs some more work)

--- www/app/home/controllers/homeController.js
+++ www/app/home/controllers/homeController.js
@@ -4,9 +4,9 @@
 
     angular.module('sparked').controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', '$rootScope', '$sce', 'ServicesDataFirebase', 'UserDataFirebase'];
+    HomeController.$inject = ['$scope', '$rootScope', '$sce', 'ServicesDataFirebase', 'UserDataFirebase', '$q'];
 
-    function HomeController($scope, $rootScope, $sce, ServicesDataFirebase, UserDataFirebase) {
+    function HomeController($scope, $rootScope, $sce, ServicesDataFirebase, UserDataFirebase, $q) {
 
         var vm = this;
 
@@ -18,9 +18,11 @@
         //80% perfect of user stack will contain selected topics; the other 20% will contain non-selected topics
         //Step 1: create user stack
         vm.userStack = [];
-        //Step 2: determine how many topics selected
-        vm.numberOfTopicsSelected = 0;
+        //Step 2: determine the  topics selected
+        vm.userTopics = [];
         //Step 3: based on number of topics selected determine what percentage of the 80% will be split among the topics
+        var computerScienceIndex = 0;
+        var mathIndex = 0;
 
         //Step 4: The news need will contain at most 20 items for now; While the selected topics stack contain data keep pushing to the user stack
 
@@ -28,23 +30,15 @@
             return $sce.trustAsResourceUrl(src);
         };
 
-        var getTopicsPromise = UserDataFirebase.getUserTopics($rootScope.currentUserPathID);
-
-        getTopicsPromise.then(function(topics) {
-
-            vm.numberOfTopicsSelected = topics.length
-
-        }, function(reason) {
-
-            console.error(reason);
-
-        });
 
         //Loading topics for new users
         var contentPromise = ServicesDataFirebase.getContent();
-        contentPromise.then(function(content) {
 
-            var allcontentArray = _.values(content);
+        var getTopicsPromise = UserDataFirebase.getUserTopics($rootScope.currentUserPathID);
+
+        $q.all([contentPromise, getTopicsPromise]).then(function(data){
+
+            var allcontentArray = _.values(data[0]);
 
             for (var i = 0; i < allcontentArray.length; i++) {
 
@@ -53,10 +47,7 @@
                     var allcontentArrayComputerScience = _.values(allcontentArray[i].computerscience);
 
                     for(var c = 0; c < allcontentArrayComputerScience.length; c++) {
-
                         vm.computerScienceStack.push({name: allcontentArrayComputerScience[c].name, videourl: allcontentArrayComputerScience[c].videourl});
-                        vm.userStack.push({name: allcontentArrayComputerScience[c].name, videourl: allcontentArrayComputerScience[c].videourl});
-
                     }
                 }
 
@@ -65,22 +56,47 @@
                     var allcontentArrayMath = _.values(allcontentArray[i].math);
 
                     for(var m = 0; m < allcontentArrayMath.length; m++) {
-
                         vm.mathStack.push({name: allcontentArrayMath[m].name, videourl: allcontentArrayMath[m].videourl});
-                        vm.userStack.push({name: allcontentArrayMath[m].name, videourl: allcontentArrayMath[m].videourl});
+                    }
+                }
 
+                if(allcontentArray[i].general) {
+
+                    var allcontentArrayGeneral = _.values(allcontentArray[i].general);
+
+                    for(var m = 0; m < allcontentArrayGeneral.length; m++) {
+                        vm.generalStack.push({name: allcontentArrayGeneral[m].name, videourl: allcontentArrayGeneral[m].videourl});
                     }
                 }
 
             }
 
-            console.log("content loaded");
+            vm.userTopics = data[1];
+            vm.numberOfItemsToDisplay = 4; //Total will be 4 * number of topics selected
+            while(vm.numberOfItemsToDisplay != 0) {
 
-        }, function(reason) {
+                for (var i = 0; i < vm.userTopics.length; i++) {
 
-            console.error(reason);
+                    if(vm.userTopics[i] === "Computer Science") {
 
+                        vm.userStack.push({name: vm.computerScienceStack[computerScienceIndex].name, videourl: vm.computerScienceStack[computerScienceIndex].videourl, startdate: vm.computerScienceStack[computerScienceIndex].startdate});
+                        computerScienceIndex++;
+                    }
+
+                    if(vm.userTopics[i] === "Math") {
+
+                        vm.userStack.push({name: vm.mathStack[mathIndex].name, videourl: vm.mathStack[mathIndex].videourl, startdate: vm.mathStack[mathIndex].startdate});
+                        mathIndex++;
+                    }
+                }
+
+                vm.numberOfItemsToDisplay--;
+            }
+
+
+            console.log(data[0], data[1]);
         });
+
 
     };
 
